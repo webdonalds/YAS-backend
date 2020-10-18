81b7dea69357893ace3be42759133a85546ccf96
hywook4 <hywook4@gmail.com>
2020-10-18 22:57:30 +0900

    Delete unused api - hello

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,9 @@
 import * as express from 'express';
 
-import hello from './v1/hello';
 import auth from './v1/auth';
 
 const router = express.Router();
 
-router.use('/v1', hello);
 router.use('/v1', auth);
 
 
