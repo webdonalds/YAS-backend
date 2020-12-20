7a57a19d529efa8dd3fbd897587e27b65729e057
hywook4 <hywook4@gmail.com>
2020-12-21 00:53:13 +0900

    Apply post api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import auth from './v1/auth';
 import user from './v1/user';
 import contents from './v1/contents';
+import post from './v1/post';
 
 import middleware from './middleware';
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.use('/v1/auth', auth);
 router.use('/v1/user', middleware.validateToken, user);
 router.use('/v1/contents', middleware.validateToken, middleware.getGoogleAccessToken, contents);
+router.use('/v1/post', post);
 
 export default router;
