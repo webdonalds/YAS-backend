8b38dcdc544d19d8074df7518cf2a6fc1c84e827
zych1751 <zych1751@naver.com>
2021-02-21 00:56:05 +0900

    fix post auth

--- src/api/index.ts
+++ src/api/index.ts
@@ -13,7 +13,8 @@ const router = express.Router();
 router.use('/v1/auth', auth);
 router.use('/v1/user', middleware.validateToken, user);
 router.use('/v1/contents', middleware.validateToken, middleware.getGoogleAccessToken, contents);
-router.use('/v1/post', middleware.validateToken, post);
+// handle middleware in post
+router.use('/v1/post', post);
 router.use('/v1/logoffed-post-list', logoffedPostList);
 
 export default router;
